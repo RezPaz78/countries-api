d1c6a9a2d5628bfa38f7d0f1494ec028111ed354
Reza Pazan <rezapazan@rocketmail.com>
2023-05-15 22:06:16 +0330

    project init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: false,
+    printWidth: 80,
+    endOfLine: "auto",
+    arrowParens: "avoid",
+    trailingComma: "es5",
+    bracketSpacing: true,
+};
